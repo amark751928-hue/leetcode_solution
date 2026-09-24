2a34ae98091adfe13b88e46657f994ad8f4925cf
Amar Kumar <amark751928@gmail.com>
2026-09-25 00:23:08 +0530

    Time: 0 ms (100%), Space: 47.8 MB (63.19%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n; 
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1.0;
+        double currentProduct = x;
+
+        while (N > 0) {
+        
+            if ((N & 1) == 1) {
+                ans *= currentProduct;
+            }
+        
+            currentProduct *= currentProduct;
+        
+            N >>= 1;
+        }
+
+        return ans;
+    }
+}
